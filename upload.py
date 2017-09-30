3c8019a3855d8a3993b9f0225dc73b8076fb2791
Hora <hena5421@gmail.com>
2017-09-30 06:55:28 -0500

    Add files via upload

--- upload.py
+++ upload.py
@@ -2,15 +2,18 @@
 import clpr as clpr
 import car_identify as caride
 import cv2
+import imageio
 
 import urllib
 import json
 import urllib.request
 
 
-def http_post(url='127.0.0.1'):
+def http_post(url='http://www.parking.com/'):
     values = PlateINFO
     jdata = json.dumps(values)
+    jdata = "plateOut="+ jdata
+    jdata = jdata.encode('utf-8')
     req = urllib.request.Request(url, jdata) # 生成页面请求的完整数据
     response = urllib.request.urlopen(req) # 发送页面请求
     return response.read()
@@ -19,8 +22,16 @@ if __name__ == '__main__':
     img_path = 'JingH99999.jpg'
     image = cv2.imread(img_path)
     # CarINFO = {}
+    # plateOut = {}
     CarINFO = caride.CarIdentify(image)
+    #print(CarINFO)
     if CarINFO['isCar']:
         PlateINFOs = clpr.clpr(img_path)
-        PlateINFO = PlateINFO[0]    
-    http_post()
+        PlateINFO = PlateINFOs[0]
+        PlateINFO['Cam_NO'] = 175
+        # plateOut['plateOut'] = PlateINFO
+        out = http_post()
+        jdata = json.dumps(PlateINFO)
+        #print("PlateINFO: ", jdata)
+    
+    print(out)
